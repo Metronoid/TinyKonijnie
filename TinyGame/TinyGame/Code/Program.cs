b987228e73d755d8e3707a78868ec767470b4653
Wander <wvdwal@hotmail.nl>
2015-10-13 09:27:57 +0200

    Renaming Game1

--- TinyGame/TinyGame/Code/Program.cs
+++ TinyGame/TinyGame/Code/Program.cs
@@ -14,7 +14,7 @@ namespace TinyGame
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new MainGame())
                 game.Run();
         }
     }
