7a8d1e2227b57b66cffa668e787bcaac41a96a0e
Wander <wvdwal@hotmail.nl>
2015-10-13 12:48:58 +0200

    GUIM fix

---
+++ TinyGame/TinyGame/GUIM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinyGame
+{
+    class GUIM
+    {
+        Konijn Player;
+
+        public void FuelMeter()
+        {
+
+        }
+        public void SpeedMeter()
+        {
+
+        }
+        public void PitMeter()
+        {
+
+        }
+        public void RoundCounter()
+        {
+
+        }
+    }
+}
