a53f7feadc9a069f66597009829de8d49bc0d77c
Henkjan <braa1201@student.nhl.nl>
2015-10-15 13:58:56 +0200

    Added font

--- TinyGame/TinyGame/Code/Objects/Blok.cs
+++ TinyGame/TinyGame/Code/Objects/Blok.cs
@@ -14,6 +14,7 @@ namespace TinyGame
         {
             this.location = location;
             this.image = image;
+            id = "blok";
             CollisionSystem.colliders.Add(this);
         }
 
@@ -24,3 +25,29 @@ namespace TinyGame
         }
     }
 }
+/*
+{
+    class trap : CollisionComponent
+    {
+        public Texture2D image;
+        public Vector2 location;
+
+        public trap(Vector2 location, Texture2D image)
+        {
+            this.location = location;
+            this.image = image;
+            id = "trap";
+            CollisionSystem.triggers.Add(this);
+        }
+
+
+
+        public void Draw(SpriteBatch sb)
+        {
+            bounds = new Rectangle((int)(location.X - image.Width / 2), (int)(location.Y - image.Height / 2), image.Width, image.Height);
+            sb.Draw(image, location, Color.White);
+        }
+    }
+
+}
+*/
