77eae5e9512b74002c6e6ad752316d6fd8214411
Wander <wvdwal@hotmail.nl>
2015-10-26 10:09:20 +0100

    Bunny Upgrade Graphics...

--- TinyGame/TinyGame/Code/Objects/Konijn.cs
+++ TinyGame/TinyGame/Code/Objects/Konijn.cs
@@ -75,12 +75,6 @@ namespace TinyGame
                 {
                     speed = -100;
                 }
-
-                if (collision == "trap")
-                {
-                    angle += 3;
-
-                }
             }
 
             velocity = new Vector2(0, 0);
